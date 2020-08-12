9c0718861999ad50e061599a5a0b96df1631732b
Pedro Cardozo <p-cardozo@hotmail.com>
2020-08-12 15:02:54 -0300

    Add Insert Into

---
+++ insert-data.sql
@@ -0,0 +1,12 @@
+use coffe_shop;
+
+SELECT * FROM products;
+
+INSERT INTO products(name, price)
+VALUES('Espresso', 2.5);
+
+INSERT INTO products(name, price)
+VALUES('Machiato', 3.00),('Cappuccino', 3.50);
+
+INSERT INTO products(name, price)
+VALUES('Cappuccino', 3.00),('Latte', 3.50),('Americano', 3.00),('Flat White', 3.50), ('Filter', 3.00);
