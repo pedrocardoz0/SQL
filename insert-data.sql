6a57c37b9c89c663c9426db99f241dba33e53120
Pedro Cardozo <p-cardozo@hotmail.com>
2020-08-13 14:15:08 -0300

    Update Fields

--- insert-data.sql
+++ insert-data.sql
@@ -10,3 +10,19 @@ VALUES('Machiato', 3.00),('Cappuccino', 3.50);
 
 INSERT INTO products(name, price)
 VALUES('Cappuccino', 3.00),('Latte', 3.50),('Americano', 3.00),('Flat White', 3.50), ('Filter', 3.00);
+
+-- Add Column --
+ALTER TABLE products
+ADD country varchar(30);
+
+-- Update Values in Columns --
+UPDATE products
+SET country = 'Brazil'
+WHERE id BETWEEN 1 and 2;
+
+UPDATE products
+SET country = 'Costa Rica'
+WHERE id = 3;
+
+-- Delete Multiple Values --
+DELETE FROM `products` WHERE id BETWEEN 15 and 20;
