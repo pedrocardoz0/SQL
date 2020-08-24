98778ab8b444f2521e0de449dea3764ac48b7a5e
Pedro Cardozo <p-cardozo@hotmail.com>
2020-08-24 14:29:28 -0300

    Add Substring

--- Cinema_Booking/substring.sql
+++ Cinema_Booking/substring.sql
@@ -1,1 +1,11 @@
 USE cinema_booking_system;
+
+SELECT SUBSTRING('Example', 3, 3) as SUB;
+
+SELECT SUBSTRING(name, 1, 3) AS short_name FROM films;
+
+SELECT SUBSTRING(name, 5, 4) AS short_name FROM films;
+
+SELECT SUBSTRING(name, -2, 2) AS short_name FROM films;
+
+SELECT SUBSTRING(name, -2) AS short_name FROM films;
